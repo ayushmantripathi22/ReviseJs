3f02bec79440f6bce90b1b9827e943129715b5de
ayushmantripathi22 <ayushmantripathi22@gmail.com>
2024-02-06 21:06:56 +0530

    p1

--- P1.js
+++ P1.js
@@ -1,5 +1,42 @@
-function addTWoNumber (number1, number2){
-    console.log(number1, number2);
-    return
+// // function addTwoNumbers(number1, number2) {
+    
+// //     return number1 + number2; 
+// // }
+
+// // let result = addTwoNumbers(3, 5);
+
+// // console.log(result); 
+
+// // function addTwoNumbers(num1,num2){
+// //     return(num1,num2)
+// // }
+// // let result = addTwoNumbers(5,8)
+// // console.log(result);
+
+// function addTwoNumbers(number1,number2){
+//     return number1+number2
+// }
+// let result = addTwoNumbers(10,5)
+// console.log(result);
+
+
+function addTwoNumbers(num1,num2){
+    return(num1+num2)
 }
-addTWoNumber(3+5);
+
+let result = addTwoNumbers(6,4)
+console.log(result);
+
+
+function MultiplyTwoNumbers(num1,num2){
+    return(num1*num2)
+}
+
+let newresult = MultiplyTwoNumbers(6,4)
+console.log(newresult);
+
+
+let ayushman = addTwoNumbers(7,8)
+let ayu= MultiplyTwoNumbers(2,2)
+console.log(`The total money given to ayushman is ${ayushman}`);
+console.log(`The total money given to ayu is ${ayu}`); 
